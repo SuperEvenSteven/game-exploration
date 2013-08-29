857683721b3b27db2514a76f638db24dd4095125
Dave Moten <davidmoten@gmail.com>
2013-08-30 08:58:31 +1000

    add serialVersionUID to DemoGame, remove unread local variable height

--- src/main/java/com/widgets/big/game/demo/DemoGame.java
+++ src/main/java/com/widgets/big/game/demo/DemoGame.java
@@ -5,6 +5,8 @@ import com.widgets.big.game.framework.Screen;
 
 public class DemoGame extends AppletGame {
 
+	private static final long serialVersionUID = -5332047309746656438L;
+
 	@Override
 	public Screen getStartScreen() {
 		return new LoadingScreen(this);
